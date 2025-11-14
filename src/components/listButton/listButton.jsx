c6e2cd6d187e2b70bae6d2fc91e9372d76267301
Tom <tommalexandre@gmail.com>
2025-11-14 16:10:26 +0100

    Almost finish visually Topbar

--- src/components/listButton/listButton.jsx
+++ src/components/listButton/listButton.jsx
@@ -2,12 +2,12 @@ import './listButton.css'
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faAngleDown } from "@fortawesome/free-solid-svg-icons";
 
-function ListButton() {
+function ListButton({ list }) {
   return (
-    <div className="listButtonContainer">
-      <p className='textListButton'>Food</p>
+    <button className="listButtonContainer">
+      <p className='textListButton'>{list[0]}</p>
       <FontAwesomeIcon icon={faAngleDown} />
-    </div>
+    </button>
   )
 }
 
--- src/components/topBar/topBar.jsx
+++ src/components/topBar/topBar.jsx
@@ -4,11 +4,16 @@ import SearchBar from "../searchBar/searchBar";
 import ListButton from "../listButton/listButton";
 
 function Topbar() {
+  const listTable = ["Food", "UserFood", "IngredientAmount", "Store"];
+  const listNumber = [5, 10, 20, 50];
+
   return (
     <div className="topBarContainer">
       <AddButton />
-      <ListButton />
+      <ListButton list={listTable}/>
       <SearchBar />
+      <p className="textResult">Result </p>
+      <ListButton list={listNumber}/>
     </div>
   )
 }
